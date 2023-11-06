a61ea6978b251244061ef5c3aa6d4f7dd2d07d42
Ricardo Sequeira <ricardosequeira4@gmail.com>
2023-11-06 17:37:23 -0400

    fix

--- controllers/subDominios/ajustes.js
+++ controllers/subDominios/ajustes.js
@@ -1,8 +1,18 @@
 import moment from 'moment'
-import { dataBasePrincipal, dataBaseSecundaria, subDominioName } from '../../constants.js'
+import { dataBaseSecundaria, subDominioName } from '../../constants.js'
 import { accessToDataBase, formatCollectionName } from '../../utils/dataBaseConfing.js'
 
 export const upsertAjusteCliente = async (req, res) => {
-  const ajuste = req.body
-  console.log(ajuste)
+  const ajuste = req.body.ajustes
+  const clienteId = req.body.clienteId
+  const db = await accessToDataBase(dataBaseSecundaria)
+  try {
+    const subDominioAjustesCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, enviromentClienteId: clienteId, nameCollection: 'ajustes' })
+    const ajustesCollection = await db.collection(subDominioAjustesCollectionsName)
+    const ajusteActualizado = await ajustesCollection.findOneAndUpdate({ tipo: ajuste.tipo }, { $set: { ...ajuste, fechaCreacion: moment().toDate() } }, { upsert: true, returnDocument: 'after' })
+    return res.status(200).json({ status: 'ajuste actualizado exitosamente', ajuste: ajusteActualizado })
+  } catch (e) {
+    console.log(e)
+    return res.status(500).json({ error: `Error de servidor al momento de actualizar ajuste ${ajuste.tipo} ${e.message}` })
+  }
 }

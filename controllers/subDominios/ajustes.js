d8a6d5c00dcd0ed51c0a8af9354e616833de4f7f
Ricardo Sequeira <ricardosequeira4@gmail.com>
2023-11-06 17:40:29 -0400

    fix

--- controllers/subDominios/ajustes.js
+++ controllers/subDominios/ajustes.js
@@ -3,7 +3,7 @@ import { dataBaseSecundaria, subDominioName } from '../../constants.js'
 import { accessToDataBase, formatCollectionName } from '../../utils/dataBaseConfing.js'
 
 export const upsertAjusteCliente = async (req, res) => {
-  const ajuste = req.body.ajustes
+  const ajuste = req.body.ajuste
   const clienteId = req.body.clienteId
   const db = await accessToDataBase(dataBaseSecundaria)
   try {

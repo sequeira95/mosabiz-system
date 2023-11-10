8e9c1e19819eaa15ba677e5cb36671b495cb4574
Ricardo Sequeira <ricardosequeira4@gmail.com>
2023-11-09 20:11:19 -0400

    dix delete

--- routes/subDominios/planCuenta.js
+++ routes/subDominios/planCuenta.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { requireSubDominioToken } from '../../middlewares/requireSubDominioToken.js'
-import { saveCuenta, getPlanCuenta } from '../../controllers/subDominios/planCuenta.js'
+import { saveCuenta, getPlanCuenta, deleteCuenta } from '../../controllers/subDominios/planCuenta.js'
 
 const router = express.Router()
 
 router.post('/getPlan', requireSubDominioToken, getPlanCuenta)
 router.post('/saveCuenta', requireSubDominioToken, saveCuenta)
+router.post('/deleteCuenta', requireSubDominioToken, deleteCuenta)
 export default router

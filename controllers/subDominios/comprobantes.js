432c2a5321c46feafcb0ef402119df57f365de24
Ricardo Sequeira <ricardosequeira4@gmail.com>
2023-11-28 18:14:48 -0400

    fix

--- controllers/subDominios/comprobantes.js
+++ controllers/subDominios/comprobantes.js
@@ -1,5 +1,5 @@
 import moment from 'moment'
-import { agreggateCollectionsSD, bulkWriteSD, createItemSD, createManyItemsSD, deleteItemSD, getItemSD, updateItemSD } from '../../utils/dataBaseConfing.js'
+import { agreggateCollectionsSD, bulkWriteSD, createItemSD, createManyItemsSD, deleteItemSD, getItemSD, updateItemSD, upsertItemSD } from '../../utils/dataBaseConfing.js'
 import { ObjectId } from 'mongodb'
 import { agregateDetalleComprobante } from '../../utils/agregateComprobantes.js'
 import { deleteImg, uploadImg } from '../../utils/cloudImage.js'
@@ -29,16 +29,15 @@ export const getListComprobantes = async (req, res) => {
 }
 
 export const createComprobante = async (req, res) => {
-  const { nombre, periodoId, clienteId } = req.body
-  if (!(nombre || !periodoId || clienteId)) return res.status(400).json({ error: 'Datos incompletos' })
+  const { comprobante, periodoId, clienteId } = req.body
+  if (!(comprobante || !periodoId || clienteId)) return res.status(400).json({ error: 'Datos incompletos' })
   try {
-    const newComprobante = await createItemSD({
+    const newComprobante = await upsertItemSD({
       nameCollection: 'comprobantes',
       enviromentClienteId: clienteId,
-      item: { nombre, periodoId: new ObjectId(periodoId), isBloqueado: false, fechaCreacion: moment().toDate() }
+      update: { $set: { ...comprobante, periodoId: new ObjectId(periodoId), isBloqueado: false, fechaCreacion: moment().toDate() } }
     })
-    const comprobante = await getItemSD({ nameCollection: 'comprobantes', enviromentClienteId: clienteId, filters: { _id: newComprobante.insertedId } })
-    return res.status(200).json({ status: 'Comprobante guardado exitosamente', comprobante })
+    return res.status(200).json({ status: 'Comprobante guardado exitosamente', comprobante: newComprobante })
   } catch (e) {
     console.log(e)
     return res.status(500).json({ error: 'Error de servidor al momento de guardar el comprobante' + e.message })
@@ -46,7 +45,6 @@ export const createComprobante = async (req, res) => {
 }
 export const updateComprobante = async (req, res) => {
   const { nombre, periodoId, clienteId, _id, isBloqueado } = req.body
-  console.log(req.body)
   if (!(nombre || !periodoId || clienteId)) return res.status(400).json({ error: 'Datos incompletos' })
   try {
     const comprobante = await updateItemSD({

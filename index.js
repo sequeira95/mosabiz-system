7784074478730b81fc0517c29c40d6086860a35a
Marco Gomez <mgomez.mosa@gmail.com>
2023-12-08 17:19:54 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(fileUpload({
   tempFileDir: './uploads' */
 }))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 // midelware
 app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser())

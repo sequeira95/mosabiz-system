52b729e090b739a827b87a7c1d14c760979a4bdd
Marco Gomez <mgomez.mosa@gmail.com>
2023-12-08 03:00:04 +0000

    Deleted routes

--- routes/subDominios/documentos.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { requireSubDominioToken } from '../../middlewares/requireSubDominioToken.js'
-import { deleteDocument, saveDocument } from '../../controllers/subDominios/documentos.js'
-
-const router = express.Router()
-
-router.post('/save', requireSubDominioToken, saveDocument)
-router.post('/delete', requireSubDominioToken, deleteDocument)
-export default router

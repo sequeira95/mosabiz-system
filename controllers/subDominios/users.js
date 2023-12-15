efd77c6eefb8962e40b1f1f2dde3cd6f4f4d065a
Ricardo Sequeira <ricardosequeira4@gmail.com>
2023-12-15 11:23:01 -0400

    fix

--- controllers/subDominios/users.js
+++ controllers/subDominios/users.js
@@ -8,12 +8,6 @@ import { ObjectId } from 'mongodb'
 
 export const getUsers = async (req, res) => {
   try {
-    /* const db = await accessToDataBase(dataBaseSecundaria)
-    const subDominioPersonasCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'personas' })
-    const personasCollection = await db.collection(subDominioPersonasCollectionsName)
-    const personas = await personasCollection.aggregate([
-      { $match: { isEmpresa: true } }
-    ]).toArray() */
     const personas = await agreggateCollectionsSD({
       nameCollection: 'personas',
       pipeline: [
@@ -28,12 +22,6 @@ export const getUsers = async (req, res) => {
 }
 export const getUsersClientes = async (req, res) => {
   try {
-    /* const db = await accessToDataBase(dataBaseSecundaria)
-    const subDominioUsersClientesCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'personas' })
-    const usersClientesCollection = await db.collection(subDominioUsersClientesCollectionsName)
-    const personas = await usersClientesCollection.aggregate([
-      { $match: { isCliente: true, clienteId: new ObjectId(req.body._id) } }
-    ]).toArray() */
     const personas = await agreggateCollectionsSD({
       nameCollection: 'personas',
       pipeline: [
@@ -49,24 +37,12 @@ export const getUsersClientes = async (req, res) => {
 export const createUser = async (req, res) => {
   const { nombre, email, clientes, telefono, modulos } = req.body
   try {
-    /* const db = await accessToDataBase(dataBaseSecundaria)
-    const subDominioUsuariosCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'usuarios' })
-    const usuariosCollection = await db.collection(subDominioUsuariosCollectionsName) */
-    // buscamos si el usuario ya existe
-    /* const verifyUser = await usuariosCollection.findOne({ email: email.toLowerCase() }) */
     const verifyUser = await getItemSD({ nameCollection: 'usuarios', filters: { email: email.toLowerCase() } })
     // en caso de que exista, retornamos un error
     if (verifyUser) return res.status(400).json({ error: 'El usuario ya se encuentra registrado' })
     const randomPassword = crypto.randomBytes(3).toString('hex')
     // encriptamos el password
     const password = await encryptPassword(randomPassword)
-    /* const userCol = await usuariosCollection.insertOne({
-      nombre,
-      email: email.toLowerCase(),
-      password,
-      fechaActPass: moment().toDate(),
-      fechaCreacion: moment().toDate()
-    }) */
     const userCol = await createItemSD({
       nameCollection: 'usuarios',
       item: {
@@ -78,18 +54,6 @@ export const createUser = async (req, res) => {
       }
     })
     const objectIdClientes = clientes.map(e => new ObjectId(e))
-    /* const subDominioPersonasCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'personas' })
-    const personasCollection = await db.collection(subDominioPersonasCollectionsName)
-    const newUser = await personasCollection.insertOne({
-      nombre,
-      email: email.toLowerCase(),
-      isEmpresa: true,
-      clientes: objectIdClientes,
-      telefono,
-      modulos,
-      usuarioId: userCol.insertedId,
-      fechaCreacion: moment().toDate()
-    }) */
     const newUser = await createItemSD({
       nameCollection: 'personas',
       item: {
@@ -103,7 +67,6 @@ export const createUser = async (req, res) => {
         fechaCreacion: moment().toDate()
       }
     })
-    /* const persona = await personasCollection.findOne({ _id: newUser.insertedId }) */
     const persona = await getItemSD({ nameCollection: 'personas', filters: { _id: newUser.insertedId } })
     // enviamos el email con el password
     const emailConfing = {
@@ -126,18 +89,6 @@ export const updateUser = async (req, res) => {
   const { _id, nombre, email, clientes, telefono, modulos } = req.body
   try {
     const objectIdClientes = clientes.map(e => new ObjectId(e))
-    /* const db = await accessToDataBase(dataBaseSecundaria)
-    const subDominioPersonasCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'personas' })
-    const personasCollection = await db.collection(subDominioPersonasCollectionsName)
-    const persona = await personasCollection.findOneAndUpdate({ _id: new ObjectId(_id) }, {
-      $set: {
-        nombre,
-        email: email.toLowerCase(),
-        clientes: objectIdClientes,
-        telefono,
-        modulos
-      }
-    }, { returnDocument: 'after' }) */
     const persona = await updateItemSD({
       nameCollection: 'personas',
       filters: { _id: new ObjectId(_id) },
@@ -151,9 +102,6 @@ export const updateUser = async (req, res) => {
         }
       }
     })
-    /* const subDominioUsuariosCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'usuarios' })
-    const usuariosCollection = await db.collection(subDominioUsuariosCollectionsName)
-    await usuariosCollection.updateOne({ _id: persona.usuarioId }, { $set: { nombre, email: email.toLowerCase() } }) */
     await updateItemSD({
       nameCollection: 'usuarios',
       filters: { _id: persona.usuarioId },
@@ -168,32 +116,7 @@ export const updateUser = async (req, res) => {
 export const createUserCliente = async (req, res) => {
   const { nombre, email, telefono, clienteId, modulos, tipoDocumento, documentoIdentidad } = req.body
   try {
-    // const db = await accessToDataBase(dataBaseSecundaria)
     const subDominioPersonasCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'personas' })
-    /* const subDominioClientesCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'clientes' })
-    const clientesCollection = await db.collection(subDominioClientesCollectionsName)
-    const cliente = await clientesCollection.aggregate([
-      { $match: { _id: new ObjectId(clienteId) } },
-      {
-        $lookup:
-          {
-            from: `${subDominioPersonasCollectionsName}`,
-            pipeline: [
-              { $match: { clienteId: new ObjectId(clienteId) } },
-              { $count: 'userLength' }
-            ],
-            as: 'personas'
-          }
-      },
-      { $unwind: { path: '$personas', preserveNullAndEmptyArrays: true } },
-      {
-        $project:
-        {
-          limiteUsuarios: '$limiteUsuarios',
-          userLength: '$personas.userLength'
-        }
-      }
-    ]).toArray() */
     const cliente = await agreggateCollectionsSD({
       nameCollection: 'clientes',
       pipeline: [
@@ -220,23 +143,12 @@ export const createUserCliente = async (req, res) => {
       ]
     })
     if (cliente[0].userLength >= cliente[0].limiteUsuarios) return res.status(400).json({ error: 'El limite de usuarios ha sido alcanzado' })
-    /* const subDominioUsuariosCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'usuarios' })
-    const usuariosCollection = await db.collection(subDominioUsuariosCollectionsName) */
-    // buscamos si el usuario ya existe
-    /* const verifyUser = await usuariosCollection.findOne({ email: email.toLowerCase() }) */
     const verifyUser = await getItemSD({ nameCollection: 'usuarios', filters: { email: email.toLowerCase() } })
     // en caso de que exista, retornamos un error
     if (verifyUser) return res.status(400).json({ error: 'El usuario ya se encuentra registrado' })
     const randomPassword = crypto.randomBytes(3).toString('hex')
     // encriptamos el password
     const password = await encryptPassword(randomPassword)
-    /* const userCol = await usuariosCollection.insertOne({
-      nombre,
-      email: email.toLowerCase(),
-      password,
-      fechaActPass: moment().toDate(),
-      fechaCreacion: moment().toDate()
-    }) */
     const userCol = await createItemSD({
       nameCollection: 'usuarios',
       item: {
@@ -247,24 +159,7 @@ export const createUserCliente = async (req, res) => {
         fechaCreacion: moment().toDate()
       }
     })
-    /* const subDominioEmpresaCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'empresa' })
-    const empresaCollection = await db.collection(subDominioEmpresaCollectionsName)
-    const empresa = await empresaCollection.findOne({}) */
     const empresa = await getItemSD({ nameCollection: 'empresa' })
-    /* const personasCollection = await db.collection(subDominioPersonasCollectionsName)
-    const newUser = await personasCollection.insertOne({
-      nombre,
-      email: email.toLowerCase(),
-      tipoDocumento,
-      documentoIdentidad,
-      isCliente: true,
-      clienteId: new ObjectId(clienteId),
-      telefono,
-      modulos,
-      usuarioId: userCol.insertedId,
-      fechaCreacion: moment().toDate(),
-      empresaId: empresa._id
-    }) */
     const newUser = await createItemSD({
       nameCollection: 'personas',
       item: {
@@ -292,7 +187,6 @@ export const createUserCliente = async (req, res) => {
       `
     }
     await senEmail(emailConfing)
-    // const persona = await personasCollection.findOne({ _id: newUser.insertedId })
     const persona = await getItemSD({ nameCollection: 'personas', filters: { _id: newUser.insertedId } })
     return res.status(200).json({ status: 'usuario creado', persona })
   } catch (e) {
@@ -303,13 +197,6 @@ export const createUserCliente = async (req, res) => {
 export const updateUserCliente = async (req, res) => {
   const { _id, nombre, email, telefono, modulos } = req.body
   try {
-    /* const db = await accessToDataBase(dataBaseSecundaria)
-    const subDominioPersonasCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'personas' })
-    const personasCollection = await db.collection(subDominioPersonasCollectionsName)
-    const persona = await personasCollection.findOneAndUpdate({ _id: new ObjectId(_id) }, { $set: { nombre, email: email.toLowerCase(), telefono, modulos } }, { returnDocument: 'after' })
-    const subDominioUsuariosCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'usuarios' })
-    const usuariosCollection = await db.collection(subDominioUsuariosCollectionsName)
-    await usuariosCollection.updateOne({ _id: persona.usuarioId }, { $set: { nombre, email: email.toLowerCase() } }) */
     const persona = await updateItemSD({
       nameCollection: 'personas',
       filters: { _id: new ObjectId(_id) },
@@ -328,15 +215,7 @@ export const updateUserCliente = async (req, res) => {
 }
 export const deleteUser = async (req, res) => {
   const { _id } = req.body
-  // const db = await accessToDataBase(dataBaseSecundaria)
   try {
-    /* const subDominioPersonasCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'personas' })
-    const personasCollection = await db.collection(subDominioPersonasCollectionsName)
-    const persona = await personasCollection.findOne({ _id: new ObjectId(_id) })
-    const personaDelete = await personasCollection.findOneAndDelete({ _id: persona._id })
-    const subDominioUsuariosCollectionsName = formatCollectionName({ enviromentEmpresa: subDominioName, nameCollection: 'usuarios' })
-    const usuariosCollection = await db.collection(subDominioUsuariosCollectionsName)
-    await usuariosCollection.deleteOne({ _id: persona.usuarioId }) */
     const persona = await getItemSD({ nameCollection: 'personas', filters: { _id: new ObjectId(_id) } })
     const personaDelete = await deleteItemSD({
       nameCollection: 'personas',

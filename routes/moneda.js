c1e8fdd9d6f13bfb04c51748e43efc62c891dbf3
Ricardo Sequeira <ricardosequeira4@gmail.com>
2024-01-04 08:16:34 -0400

    fix router monedas

--- routes/moneda.js
+++ routes/moneda.js
@@ -1,13 +1,12 @@
 import express from 'express'
 import { requireToken } from '../middlewares/requireToken.js'
 import { deleteMoneda, getMonedas, getTasas, saveMoneda } from '../controllers/monedas.js'
-import { requireSubDominioToken } from '../middlewares/requireSubDominioToken.js'
 
 const router = express.Router()
 
-router.post('/get', (requireToken || requireSubDominioToken), getMonedas)
+router.post('/get', getMonedas)
 router.post('/save', requireToken, saveMoneda)
 router.post('/delete', requireToken, deleteMoneda)
-router.post('/getTasas', (requireToken || requireSubDominioToken), getTasas)
+router.post('/getTasas', getTasas)
 
 export default router

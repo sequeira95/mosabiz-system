a3204c9cfc1bc331f3320086ddd0eac285e8a22b
Ricardo Sequeira <ricardosequeira4@gmail.com>
2024-09-24 16:49:15 -0400

    fix

--- controllers/subDominios/administracion/tesoria.js
+++ controllers/subDominios/administracion/tesoria.js
@@ -162,27 +162,221 @@ export const getTotalesTransaciones = async (req, res) => {
     }, [
       {
         tipo: 'Efectivo ' + monedaPrincipal,
-        total: 0
+        total: 0,
+        tipoBusqueda: 'EN' // EFECTIVO NACIONAL
       },
       {
         tipo: 'Efectivo Divisas',
-        total: 0
+        total: 0,
+        tipoBusqueda: 'ED' // EFECTIVO EN DIVISAS
       },
       {
         tipo: 'Bancos Nacionales ' + monedaPrincipal,
-        total: 0
+        total: 0,
+        tipoBusqueda: 'BN' // BANCOS NACIONALES
       },
       {
         tipo: 'Bancos Nacionales Divisas',
-        total: 0
+        total: 0,
+        tipoBusqueda: 'BND' // BANCOS NACIONALES EN DIVISAS
       },
       {
         tipo: 'Bancos Internacionales Divisas',
-        total: 0
+        total: 0,
+        tipoBusqueda: 'BI' // BANCOS INTERNACIONALES
       }
     ])
     console.log({ totales })
     return res.status(200).json({ tasa, totales })
+  } catch (e) {
+    console.log(e)
+    return res.status(500).json({ error: 'Error de servidor al momento de buscar información sobre las transacciones ' + e.message })
+  }
+}
+export const getDetalleTransacciones = async (req, res) => {
+  const { clienteId, tipoBusqueda, fechaTasa, fecha, monedaPrincipal } = req.body
+  try {
+    let tasa = await getItem({ nameCollection: 'tasas', filters: { fechaUpdate: fechaTasa, monedaPrincipal } })
+    if (!tasa) {
+      const ultimaTasa = await agreggateCollections({
+        nameCollection: 'tasas',
+        pipeline: [
+          { $sort: { fechaOperacion: -1 } },
+          { $limit: 1 }
+        ]
+      })
+      tasa = ultimaTasa[0] ? ultimaTasa[0] : null
+    }
+    const transaccionesCollection = formatCollectionName({ enviromentEmpresa: subDominioName, enviromentClienteId: clienteId, nameCollection: 'transacciones' })
+    let tiposBusqueda = { }
+    if (tipoBusqueda === 'EN') {
+      tiposBusqueda = {
+        tipo: 'Nacional',
+        isNacionalDivisas: { $ne: true },
+        $or: [
+          { tipoBanco: 'cajaChica' },
+          { tipoBanco: 'cajaPrincipal' }
+        ]
+      }
+    }
+    if (tipoBusqueda === 'ED') {
+      tiposBusqueda = {
+        $or: [
+          {
+            tipo: 'Nacional',
+            tipoBanco: 'cajaChica',
+            isNacionalDivisas: true
+          },
+          {
+            tipo: 'Nacional',
+            tipoBanco: 'cajaPrincipal',
+            isNacionalDivisas: true
+          },
+          {
+            tipo: 'Internacional',
+            tipoBanco: 'cajaPrincipal'
+          },
+          {
+            tipo: 'Internacional',
+            tipoBanco: 'cajaChica'
+          }
+        ]
+      }
+    }
+    if (tipoBusqueda === 'BN') {
+      tiposBusqueda = {
+        tipo: 'Nacional',
+        tipoBanco: 'banco',
+        isNacionalDivisas: { $ne: true }
+      }
+    }
+    if (tipoBusqueda === 'BND') {
+      tiposBusqueda = {
+        tipo: 'Nacional',
+        tipoBanco: 'banco',
+        isNacionalDivisas: true
+      }
+    }
+    if (tipoBusqueda === 'BI') {
+      tiposBusqueda = {
+        tipo: 'Internacional',
+        tipoBanco: 'banco'
+      }
+    }
+    const detalle = await agreggateCollectionsSD({
+      nameCollection: 'bancos',
+      enviromentClienteId: clienteId,
+      pipeline: [
+        { $match: tiposBusqueda },
+        {
+          $lookup: {
+            from: transaccionesCollection,
+            let: { cajaBancoId: '$_id' },
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $or: [
+                      { $eq: ['$caja', '$$cajaBancoId'] },
+                      { $eq: ['$banco', '$$cajaBancoId'] }
+                    ]
+                  }
+                }
+              },
+              {
+                $addFields: {
+                  tasa: { $objectToArray: tasa }
+                }
+              },
+              { $unwind: { path: '$tasa', preserveNullAndEmptyArrays: true } },
+              { $match: { $expr: { $eq: ['$tasa.k', '$monedaSecundaria'] } } },
+              {
+                $addFields: {
+                  valor: { $multiply: ['$tasa.v', '$pagoSecundario'] }
+                }
+              },
+              {
+                $group: {
+                  _id: null,
+                  ingresos: {
+                    $sum: {
+                      $cond: {
+                        if: {
+                          $and: [
+                            { $eq: ['$tipo', 'venta'] },
+                            { $ne: ['$tipoDocumento', tiposDocumentosFiscales.notaCredito] },
+                            { $ne: ['$tipoDocumento', tiposDocumentosFiscales.devolucion] }
+                          ]
+                        },
+                        then: '$valor',
+                        else: 0
+                      }
+                    }
+                  },
+                  egresosVentas: {
+                    $sum: {
+                      $cond: {
+                        if: {
+                          $or: [
+                            {
+                              $and: [
+                                { $eq: ['$tipo', 'venta'] },
+                                { $eq: ['$tipoDocumento', tiposDocumentosFiscales.notaCredito] }
+                              ]
+                            },
+                            {
+                              $and: [
+                                { $eq: ['$tipo', 'venta'] },
+                                { $eq: ['$tipoDocumento', tiposDocumentosFiscales.devolucion] }
+                              ]
+                            }
+                          ]
+                        },
+                        then: '$valor',
+                        else: 0
+                      }
+                    }
+                  },
+                  egresosCompras: {
+                    $sum: {
+                      $cond: {
+                        if: {
+                          $and: [
+                            { $eq: ['$tipo', 'compra'] },
+                          ]
+                        },
+                        then: '$valor',
+                        else: 0
+                      }
+                    }
+                  }
+                }
+              },
+              {
+                $project: {
+                  _id: 0,
+                  ingresos: 1,
+                  egresos: { $add: ['$egresosVentas', '$egresosCompras'] },
+                  saldo: { $subtract: ['$ingresos', { $add: ['$egresosVentas', '$egresosCompras'] }] }
+                }
+              }
+            ],
+            as: 'detalleTransacciones'
+          }
+        },
+        { $unwind: { path: '$detalleTransacciones', preserveNullAndEmptyArrays: true } },
+        {
+          $project: {
+            nombre: 1,
+            descripcion: 1,
+            ingresos: '$detalleTransacciones.ingresos',
+            egresos: '$detalleTransacciones.egresos',
+            saldo: '$detalleTransacciones.saldo'
+          }
+        }
+      ]
+    })
+    return res.status(200).json({ detalle })
   } catch (e) {
     console.log(e)
     return res.status(500).json({ error: 'Error de servidor al momento de buscar información sobre las transacciones ' + e.message })
--- routes/subDominios/administracion.js
+++ routes/subDominios/administracion.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { requireSubDominioToken } from '../../middlewares/requireSubDominioToken.js'
-import { getTotalesTransaciones } from '../../controllers/subDominios/administracion/tesoria.js'
+import { getDetalleTransacciones, getTotalesTransaciones } from '../../controllers/subDominios/administracion/tesoria.js'
 
 const router = express.Router()
 
 router.post('/get/totalesTransacciones', requireSubDominioToken, getTotalesTransaciones)
+router.post('/get/DetalleTransacciones', requireSubDominioToken, getDetalleTransacciones)
 export default router
